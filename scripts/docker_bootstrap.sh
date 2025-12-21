f17470190c91deb19a3ba8057c24ee32adb8b96f
Yu-Tsung Lee <eddy82814@MacBook-Air.local>
2025-12-21 14:43:43 -0800

    Use modern Gradle in Docker image

--- scripts/docker_bootstrap.sh
+++ scripts/docker_bootstrap.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 python3 -m pip install --upgrade pip
 python3 -m pip install -e .
 
+gradle -v
+
 gradle -p java/soot-extractor jar
 
 mvn -f FlowDroid/pom.xml -pl soot-infoflow-cmd -am package -DskipTests
