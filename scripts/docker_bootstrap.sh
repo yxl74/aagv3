05e805ccac249692339f8139aa4003f348afcd1e
Yu-Tsung Lee <eddy82814@MacBook-Air.local>
2025-12-21 15:10:52 -0800

    Build FlowDroid release jar in docker bootstrap by default

--- scripts/docker_bootstrap.sh
+++ scripts/docker_bootstrap.sh
@@ -8,4 +8,16 @@ gradle -v
 
 gradle -p java/soot-extractor jar
 
-mvn -f FlowDroid/pom.xml -pl soot-infoflow-cmd -am package -DskipTests
+FLOWDROID_BUILD_MODE="${FLOWDROID_BUILD_MODE:-release}"
+FLOWDROID_TAG="${FLOWDROID_TAG:-v2.14.1}"
+
+if [[ "${FLOWDROID_BUILD_MODE}" == "release" ]]; then
+  tmpdir="$(mktemp -d)"
+  git clone --depth 1 --branch "${FLOWDROID_TAG}" https://github.com/secure-software-engineering/FlowDroid.git "${tmpdir}"
+  mvn -f "${tmpdir}/pom.xml" -pl soot-infoflow-cmd -am package -DskipTests
+  mkdir -p FlowDroid/soot-infoflow-cmd/target
+  cp "${tmpdir}/soot-infoflow-cmd/target/soot-infoflow-cmd-jar-with-dependencies.jar" \
+    FlowDroid/soot-infoflow-cmd/target/
+else
+  mvn -f FlowDroid/pom.xml -pl soot-infoflow-cmd -am package -DskipTests
+fi
