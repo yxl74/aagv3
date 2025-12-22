0792f42dd29a1d936532eb230b73fafe09465916
Eddy Lee <37917160+yxl74@users.noreply.github.com>
2025-12-21 22:55:30 -0800

    Preinstall requirements in docker image

--- scripts/docker_entrypoint.sh
+++ scripts/docker_entrypoint.sh
@@ -4,7 +4,9 @@ set -euo pipefail
 export PYTHONPATH="/workspace/src:${PYTHONPATH:-}"
 
 if [[ "${AAG_SKIP_PIP_INSTALL:-}" != "1" ]] && [[ -f /workspace/requirements.txt ]]; then
-  python -m pip install -r /workspace/requirements.txt
+  if ! python -c "import androguard" >/dev/null 2>&1; then
+    python -m pip install -r /workspace/requirements.txt
+  fi
 fi
 
 exec "$@"
